3c3c9b8fd484e4f8ad296609e3e3e381428f68c0
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-07-07 10:49:13 +0200

    poczatek filtrów

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, TextField } from '@material-ui/core';
 
-{/* jakość, trudność, czas przygotowania */}
+{/* jakość, trudność, czas przygotowania */ }
 class filters extends Component {
 	render() {
 		return (
 			<Grid container justify="space-around">
-				<Typography variant="subheading">Filtry: TODO</Typography>
+				<Typography variant="subheading">Filtry: </Typography>
+				<form>
+					<TextField 
+						label="Min. ocena jakości"/>
+				</form>
 			</Grid>
 		);
 	}
